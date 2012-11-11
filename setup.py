b5057ad69e2cbb0db12aff56e444e71b08189885
Krzysztof Dorosz <cypreess@gmail.com>
2012-11-11 20:29:31 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='python-mogilefs',
+    version='1.0',
+    packages=['mogilefs'],
+    url='http://www.albany.edu/~ja6447/mogilefs.py',
+    license='GPL',
+    author='Justin Azoff',
+    author_email='',
+    description='Justin Azoff',
+    install_requires=['pycurl==7.19.0']
+
+)
